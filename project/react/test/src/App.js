73f15f593ab07a5beb8cfd4367101374aec24dcc
tuy112 <master_catch@naver.com>
2023-05-02 16:09:41 +0900

    2023.05.02

---
+++ project/react/test/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  let title = "연습장입니다";
+
+  return (
+    <div className = "App">
+      <div class = "blackNav">
+        <h3>Jstory - JH</h3>
+      </div>
+      <h3>{title}</h3>
+    </div>
+  );
+}
+
+export default App;
